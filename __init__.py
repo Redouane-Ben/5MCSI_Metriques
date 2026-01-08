dcd6c68341574f8267cdb6fdfcf183cdfe88b8ac
Redouane-Ben <benamar.redouane@hotmail.fr>
2026-01-08 10:03:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,14 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)     
+
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
   @app.route('/tawarano/')
+
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
@@ -20,6 +23,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm1
